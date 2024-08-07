cb4d68cc7a3eea5f1738f992803de7bb927a24fb
Rob Scanlon <robscanlon@gmail.com>
2024-08-07 17:11:10 -0400

    Update to final rule and maintenance requirements.

--- lib/service_base_url_test_kit.rb
+++ lib/service_base_url_test_kit.rb
@@ -7,49 +7,54 @@ module ServiceBaseURLTestKit
     id :service_base_url
     title 'Service Base URL Test Suite'
     description %(
-      This test kit provides a draft set of tests to validate conformance to
-      [HTI-1](https://www.healthit.gov/topic/laws-regulation-and-policy/health-data-technology-and-interoperability-certification-program)
-      [rule](https://www.ecfr.gov/current/title-45/subtitle-A/subchapter-D/part-170/subpart-D/section-170.404#p-170.404(b)(2))
-      from the API Condition and Maintenance of Certification to include the
-      requirement for Certified API Developers with patient-facing apps to
-      publish their service base URLs in [a specified
-      format](https://www.federalregister.gov/d/2023-07229/p-2342).
+      This Test Kit provides a set of tests that verifies conformance of Service
+      Base URL publications to data format requirements as described in
+      [Conditions of Maintenance of Certification - Application programming
+      interfaces](https://www.ecfr.gov/current/title-45/subtitle-A/subchapter-D/part-170/subpart-D/section-170.404#p-170.404(b)(2))
+      and the [ONC HTI-1 Final
+      Rule](https://www.healthit.gov/topic/laws-regulation-and-policy/health-data-technology-and-interoperability-certification-program).
+      Please review the [Application Programming Interfaces Certification Companion
+      Guide](https://www.healthit.gov/condition-ccg/application-programming-interfaces)
+      for additional guidance.
 
-      A Certified API developer must publish, at no charge, the service base
-      URLs and related organizational details that can be used by patients to
-      access their electronic health information. This test kit will test that a
-      server's service base URL list conforms to the following:
-        - Service base URL list is publicly accessible
-        - Service based URLs are published in the Endpoint resource format
-          according to the standard adopted in § 170.215\(a\) - FHIR 4.0.1 release
-        - Organization details for each service base URL are published in the
-          Organization resource format according to the standard adopted in §
-          170.215\(a\) - FHIR 4.0.1 release
-        - Each Endpoint resource must:
-          - Have at least one Organization resource that references it in the Bundle
-        - Each Organization resource must:
-          - Have a populated Organization.endpoint field that contains
-            references to the Endpoint resources containing service base URLs
-            managed by this organization
-          - Contain the organization's name, location, and provider identifier 
-        - Endpoint and Organization resources must be:
-          - Collected into a Bundle resource formatted according to the standard
-            adopted in FHIR v4.0.1: § 170.215\(a\) for publication
-
-      While these tests do not specifically verify conformance to
-      [Patient-Access
-      Brands](https://build.fhir.org/ig/HL7/smart-app-launch/brands.html) within
-      the draft SMART App Launch v2.2.0 standard, systems that implement that
-      standard should pass these tests. Please report an issue if there are any
-      problems.
-
-      The tests within this test kit are available for developers that would
-      like to evaluate their service list against the specified format. This is
-      a draft set of tests and may contain errors or issues. Please provide
-      feedback on these tests by reporting an issue in
+      This Test Kit is provided as a tool to help developers identify potential issues
+      or problems with the structure of their Service Base URL publication.  Test
+      failures do not necessarily indicate non-conformance to the Conditions of
+      Maintenance of Certification.  Use of these tests is not required.  Please
+      provide feedback on these tests by reporting an issue in
       [GitHub](https://github.com/inferno-framework/service-base-url-test-kit/issues),
       or by reaching out to the team on the [Inferno FHIR Zulip
       channel](https://chat.fhir.org/#narrow/stream/179309-inferno).
+
+      Relevant requirements from the Conditions of [Maintenance of Certification -
+      Application programming interfaces](https://www.ecfr.gov/current/title-45/subtitle-A/subchapter-D/part-170/subpart-D/section-170.404#p-170.404(b)(2)):
+
+      Service Base URL publication:
+
+      For all Health IT Modules certified to § 170.315(g)(10), a Certified API
+      Developer must publish, at no charge, the service base URLs and related
+      organization details that can be used by patients to access their
+      electronic health information, by December 31, 2024. This includes all
+      customers regardless of whether the Health IT Modules certified to §
+      170.315(g)(10) are centrally managed by the Certified API Developer or
+      locally deployed by an API Information Source. These service base URLs and
+      organization details must conform to the following:
+      
+       - Service base URLs must be publicly published in Endpoint resource format
+         according to the standard adopted in § 170.215(a) (FHIR v4.0.1).
+       - Organization details for each service base URL must be publicly published in Organization
+         resource format according to the standard adopted in § 170.215(a) (FHIR v4.0.1). Each
+         Organization resource must contain: 
+          - A reference, in the Organization endpoint element, to the Endpoint
+            resources containing service base URLs managed by this organization.
+          - The organization’s name, location, and facility identifier.
+       - Endpoint and Organization resources must be:
+         - Collected into a Bundle resource formatted according to the standard
+            adopted in § 170.215(a) (FHIR v4.0.1) for publication; 
+         - and Reviewed quarterly and, as
+            necessary, updated.
+
+
     )
     version VERSION
 
--- lib/service_base_url_test_kit/service_base_url_test_group.rb
+++ lib/service_base_url_test_kit/service_base_url_test_group.rb
@@ -6,14 +6,14 @@ module ServiceBaseURLTestKit
     id :service_base_url_test_group
     title 'Retrieve and Validate Service Base URL List'
     description %(    
-    Verify that the developer makes its Service Base URL list publicly available
+    Verify that the developer makes its Service Base URL publication is publicly available
     in the Bundle resource format with valid Endpoint and Organization entries.
     This test group will issue a HTTP GET request against the supplied URL to
     retrieve the developer's Service Base URL list and ensure the list is
     publicly accessible. It will then ensure that the returned service base URL
-    list is in the Bundle resource format containing its service base URLs and
+    publication is in the Bundle resource format containing its service base URLs and
     related organizational details in valid Endpoint and Organization resources
-    that follow the specifications detailed in the HTI-1 rule in the API
+    that follow the specifications detailed in the HTI-1 rule and the API
     Condition and Maintenance of Certification.
 
     For systems that provide the service base URL Bundle at a URL, please run
