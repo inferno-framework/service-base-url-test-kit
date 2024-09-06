f6941d710a17902e70155c041357f8ab9eac82a4
Rob Scanlon <robscanlon@gmail.com>
2024-09-06 16:02:12 -0400

    FI-3036 Fix minor typo in readme and suite description. (#28)
    * Fix minor typo in readme and suite description.
    
    * Refine language in readme and suite description.

--- lib/service_base_url_test_kit.rb
+++ lib/service_base_url_test_kit.rb
@@ -9,7 +9,7 @@ module ServiceBaseURLTestKit
     description %(
       This Test Kit provides a set of tests that verify conformance of Service
       Base URL publications to data format requirements as described in
-      [Conditions of Maintenance of Certification - Application programming
+      [Conditions and Maintenance of Certification - Application programming
       interfaces](https://www.ecfr.gov/current/title-45/subtitle-A/subchapter-D/part-170/subpart-D/section-170.404#p-170.404(b)(2))
       and the [ONC HTI-1 Final
       Rule](https://www.healthit.gov/topic/laws-regulation-and-policy/health-data-technology-and-interoperability-certification-program).
@@ -17,17 +17,19 @@ module ServiceBaseURLTestKit
       Guide](https://www.healthit.gov/condition-ccg/application-programming-interfaces)
       for additional guidance.
 
-      This Test Kit is provided as a tool to help developers identify potential issues
-      or problems with the structure of their Service Base URL publication.  Test
-      failures do not necessarily indicate non-conformance to the Conditions of
-      Maintenance of Certification.  Use of these tests is not required.  Please
-      provide feedback on these tests by reporting an issue in
+      This Test Kit is provided as a tool to help developers identify potential
+      issues or problems with the structure of their Service Base URL
+      publication.  Test failures do not necessarily indicate non-conformance to
+      the Conditions and Maintenance of Certification.  Use of these tests is
+      not required for the participants of the ONC Health IT Certification Program.
+      Please provide feedback on these tests by reporting an issue in
       [GitHub](https://github.com/inferno-framework/service-base-url-test-kit/issues),
       or by reaching out to the team on the [Inferno FHIR Zulip
       channel](https://chat.fhir.org/#narrow/stream/179309-inferno).
-
-      Relevant requirements from the Conditions of [Maintenance of Certification -
-      Application programming interfaces](https://www.ecfr.gov/current/title-45/subtitle-A/subchapter-D/part-170/subpart-D/section-170.404#p-170.404(b)(2)):
+      
+      Relevant requirements from the [Conditions and Maintenance of
+      Certification - Application programming
+      interfaces](https://www.ecfr.gov/current/title-45/subtitle-A/subchapter-D/part-170/subpart-D/section-170.404#p-170.404(b)(2)):
 
       Service Base URL publication:
 
