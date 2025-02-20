855a8574d4d645e382d6a241c859415c99350e66
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-02-20 13:53:03 -0500

    impl platform deployment spec wip

---
+++ lib/service_base_url_test_kit/metadata.rb
@@ -0,0 +1,16 @@
+module ServiceBaseURLTestKit
+  class Metadata < Inferno::TestKit
+    id :service_base_url_test_kit
+    title 'Service Base Url Test Kit'
+    suite_ids ['service_base_url']
+    tags ['Endpoint Publication']
+    last_updated ::ServiceBaseURLTestKit::LAST_UPDATED
+    version ::ServiceBaseURLTestKit::VERSION
+    maturity 'Medium'
+    authors ['Inferno Team']
+    repo 'https://github.com/inferno-framework/service-base-url-test-kit'
+    description <<~DESCRIPTION
+      TODO
+    DESCRIPTION
+  end
+end
