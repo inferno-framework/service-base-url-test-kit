1a2ee426bd2712b4112cd684496b9d4234e540d1
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-02-25 14:57:21 -0500

    bump version, last_updated; bundle lock

--- lib/service_base_url_test_kit/version.rb
+++ lib/service_base_url_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module ServiceBaseURLTestKit
-  VERSION = '0.11.1'          # TODO breaking change
-  LAST_UPDATED = '2024-11-06'
+  VERSION = '0.12.0'
+  LAST_UPDATED = '2025-02-15'
 end
